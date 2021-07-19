c75a25e4611fed02f39ac4af7606cbf33e0b5155
Deeb <mcdeebt@gmail.com>
2021-07-19 23:37:26 +0300

    new commit

---
+++ src/components/menuItems.js
@@ -0,0 +1,12 @@
+const menuNames = [
+    {
+        title: 'Home',
+        url: '#',
+        cname: 'Nav-Links'
+    },
+    {
+        title: 'Vote',
+        url: '#',
+        cname: 'Nav-Links'
+    },
+]
